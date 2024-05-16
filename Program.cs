84987fcae64806df1120782992c451225400db58
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-16 01:49:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Welcome Message
+Console.WriteLine("Welcome to the sample calculator!");
+
+// Prompt user for input
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Show Calculator Options
+
+// Decide which operation is needed based on selected option
+
+// print output
+
+
