54c5746a731a1b22ed798819019c14d6e3e7cd74
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-16 02:25:21 -0400

    started sample calculator

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@
 // Welcome Message
 Console.WriteLine("Welcome to the sample calculator!");
 
+
+// Show Calculator Options
+Console.WriteLine("Please select an operation");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int operation = Convert.ToInt32(Console.ReadLine());
+
+
 // Prompt user for input
 Console.Write("Please enter the first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -10,10 +20,32 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Please enter the second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-// Show Calculator Options
-
 // Decide which operation is needed based on selected option
 
-// print output
+int resultOfOutput = 0;
+switch (operation)
+{
+    case 1:
+        int sum = num1 + num2;
+        resultOfOutput = sum;
+        break;
+    case 2:
+        int difference = num1 - num2;
+        resultOfOutput = difference;
+        break;
+    case 3:
+        int product = num1 * num2;
+        resultOfOutput = product;
+        break;
+    case 4:
+        int quotient = num1 / num2;
+        resultOfOutput = quotient;
+        break;
+    default:
+        Console.WriteLine("Invalid Choice");
+        break;
+}
 
+// print output
+Console.WriteLine($" the solution is {resultOfOutput}");
 
